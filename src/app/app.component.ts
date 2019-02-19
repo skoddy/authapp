36c105c3660a02c868174fabc2bd7f6eeceb960c
skoddy <m.t.skodowski@gmail.com>
2019-02-19 12:35:25 +0100

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { Overlay, OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @HostBinding('class') componentCssClass;
+
   title = 'myapp';
+  theme: string;
+  
+  constructor(public overlay: Overlay, public overlayContainer: OverlayContainer) {
+    this.setTheme();
+  }
+  setTheme(): any {
+    this.theme = 'dark-theme';//  'default-theme';
+    this.componentCssClass = this.theme;
+    const classList = this.overlayContainer.getContainerElement().classList;
+    const toRemove = Array.from(classList).filter((item: string) =>
+      item.includes('-theme')
+    );
+    classList.remove(...toRemove);
+    classList.add(this.theme);
+  }
 }
--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -8,13 +9,19 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
 export class RegisterComponent implements OnInit {
   hide = true;
+
+  nameFormControl = new FormControl('', [
+    Validators.required
+  ]);
   
   emailFormControl = new FormControl('', [
     Validators.required,
