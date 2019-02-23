e8953890fae12a68c51a809371ecb76289c10900
skoddy <m.t.skodowski@gmail.com>
2019-02-23 16:55:31 +0100

    autofill

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { AutofillMonitor } from '@angular/cdk/text-field';
 
 export class PasswordMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl): boolean {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
     const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
-    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty && (control.touched || control.dirty));
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty && (control.touched || control.dirty || isSubmitted));
 
     return (invalidCtrl || invalidParent);
   }
 }
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -23,6 +20,9 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 
 export class RegisterComponent implements OnInit {
+  @ViewChild('name', { read: ElementRef }) name: ElementRef<HTMLElement>;
+  @ViewChild('email', { read: ElementRef }) email: ElementRef<HTMLElement>;
+
   hide = true;
   registerForm = new FormGroup({
     name: new FormControl('', [
@@ -42,12 +42,38 @@ export class RegisterComponent implements OnInit {
 
   matcher = new PasswordMatcher();
 
-  constructor() { }
+  constructor(private autofill: AutofillMonitor, private elem: ElementRef) { }
 
   ngOnInit() {
+    this.autofill.monitor(this.name)
+      .subscribe(e => {
+        this.customAutofill(e, this.name);
+      });
+    this.autofill.monitor(this.email)
+      .subscribe(e => {
+        this.customAutofill(e, this.email);
+      });
+  }
+  ngAfterViewInit() {
+    // you'll get your through 'elements' below code
+
+  }
+  ngOnDestroy() {
+    this.autofill.stopMonitoring(this.name);
+    this.autofill.stopMonitoring(this.email);
   }
   get f() { return this.registerForm.controls; }
-
+  customAutofill(event, element: ElementRef<HTMLElement>) {
+    const firstChild = element.nativeElement.parentElement.parentElement.firstElementChild.classList;
+    const secondChild = element.nativeElement.parentElement.parentElement.children[1].classList;
+    if (event.isAutofilled) {
+      firstChild.add('custom-autofill');
+      secondChild.add('custom-autofill');
+    } else {
+      firstChild.remove('custom-autofill');
+      secondChild.remove('custom-autofill');
+    }
+  }
   passwordMatchValidator(g: FormControl) {
     return g.get('password').value === g.get('repeat').value
       ? null : { 'mismatch': true };
